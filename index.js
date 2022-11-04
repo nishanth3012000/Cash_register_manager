c07c7893b61b2288f08e1242afd0f0cffb16ff2d
lebo2003012000 <114799682+nishanth3012000@users.noreply.github.com>
2022-11-04 15:57:41 +0530

    upto calc

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll("#no-of-notes");
 
-addEventListener.checkButton("click", () => console.log("clicked");
-//  function validateBillAmount(){
-//     if(billAmount.value > 0){
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 
-//     }else{
-//         message.innerText = "The bill amount should be greater than 0";
-//     }
+checkButton.addEventListener("click",  function validateBillAmount(){
+    if(billAmount.value > 0){
+        if(billAmount.value < cashGiven.value ){
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateAmountToBeReturned(amountToBeReturned)
+        }else{
+            message.innerText ="we have job in the kitchen"
+        }
 
-// });
+
+    }else{
+        message.innerText = "The bill amount should be greater than 0";
+    }
+
+});
+
+function calculateAmountToBeReturned{
+
+
+}
 
 
 
