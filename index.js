e67e33cd12c8e788c683631fa32533c98f64be39
lebo2003012000 <114799682+nishanth3012000@users.noreply.github.com>
2022-11-04 22:18:34 +0530

    update

--- index.js
+++ index.js
@@ -8,25 +8,40 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 
 checkButton.addEventListener("click",  function validateBillAmount(){
+    hideMessage()
     if(billAmount.value > 0){
-        if(billAmount.value < cashGiven.value ){
+        if(cashGiven.value >= billAmount.value ){
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateAmountToBeReturned(amountToBeReturned)
         }else{
-            message.innerText ="we have job in the kitchen"
+            showMessage("we have job in the kitchen")
         }
 
 
     }else{
-        message.innerText = "The bill amount should be greater than 0";
+        showMessage("Invalid bill amount") 
     }
 
 });
 
-function calculateAmountToBeReturned{
+function calculateAmountToBeReturned(amountToBeReturned){
+    for(let i =0; i < availableNotes.length; i++){
 
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[0]);   // no of notes need for the denomination
 
+        amountToBeReturned = amountToBeReturned % availableNotes;  // amount left after calculating the number of notes needed
+
+        noOfNotes[i].innerText = numberOfNotes;   // updating the no of notes in the table for the current amount            
+    }
 }
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg
+}
+function hideMessage(){
+    message.style.display = "none";
+}
+
 
 
 
